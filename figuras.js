52be13b82e1422c7b2f536129f54ba6b3b850c27
Javier Mojito <jcollazosgomez@gmail.com>
2021-09-27 17:03:04 -0500

    Agregando conexion entre HMTL y JS en metodos de cuadrado y triangulo

--- figuras.js
+++ figuras.js
@@ -26,12 +26,11 @@ console.groupEnd();
 
 //------- Código del Círculo -------//
 console.group("Círculo");
+const PI = Math.PI;
 
 function diametroCirculo(radio) {
     return radio * 2;
 }
-
-const PI = Math.PI;
 
 function perimetroCirculo(radio) {
     return diametroCirculo(radio) * PI;
@@ -42,3 +41,47 @@ function areaCirculo(radio) {
 }
 
 console.groupEnd();
+
+// Aquí interactuamos con el HTML
+//--- Cuadrado ---//
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+//--- Triangulo ---//
+function calcularPerimetroTriangulo() {
+    const input1 = document.getElementById("lado1Triangulo");
+    const lado1 = input1.value;
+
+    const input2 = document.getElementById("lado2Triangulo");
+    const lado2 = input2.value;
+
+    const input3 = document.getElementById("baseTriangulo");
+    const base = input3.value;
+
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo() {
+    const input1 = document.getElementById("baseTriangulo");
+    const base = input1.value;
+
+    const input2 = document.getElementById("alturaTriangulo");
+    const altura = input2.value;
+
+    const area = areaTriangulo(base, altura);
+    alert(area);
+}
